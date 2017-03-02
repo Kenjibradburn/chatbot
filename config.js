9dcac9c2332fc55843d3be085447efc692e6fd3f
Kenjibradburn <kenjibradburn@gmail.com>
2017-03-02 17:45:42 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAZAwpORkYtUBADzV5VlnJ4xSjYmFUcFnqaqoBRBLl7s989fcSxpLLDTfPZCfZCyjXJRDZB576rlVUL98B1NV7kBZAYcDCmUtybZCZCeny8dYT3BSWg6KnBxgY0GAXvtnrzXBKQFgoXmXZB5SnrwYt4qNQonxjQ7JpR9VuEnAcl41AZDZD',
+    FB_VERIFY_TOKEN: '',
+    API_AI_CLIENT_ACCESS_TOKEN: '',
+    FB_APP_SECRET: 'Bradburn2017',
+    SERVER_URL: "https://polar-eyrie-86548.herokuapp.com/webhook",
+};
