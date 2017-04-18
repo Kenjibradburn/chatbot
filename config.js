e43d5b592982b66b072b42b211603e606b5abf12
Kenjibradburn <kenjibradburn@gmail.com>
2017-04-18 17:16:53 +0200

    Update config.js
    Updated FB token

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAAZAwpORkYtUBADzV5VlnJ4xSjYmFUcFnqaqoBRBLl7s989fcSxpLLDTfPZCfZCyjXJRDZB576rlVUL98B1NV7kBZAYcDCmUtybZCZCeny8dYT3BSWg6KnBxgY0GAXvtnrzXBKQFgoXmXZB5SnrwYt4qNQonxjQ7JpR9VuEnAcl41AZDZD',
+    FB_PAGE_TOKEN: 'EAAD8JXH3yx8BAHRZC4VNJt7RDaWih1J5ON2EW1UI22ZBFeYo0id7FTaoCKpSJqYeysZAK9h5uzOzIdLMuKcdzJkySU5MosbZBmHwrZCpLryni01E2o0Xmab2WR5F3iWt9JKR2NUDhHEuGoOxClSQDU2rut3xuY0nNxKdAw09yaQZDZD',
     FB_VERIFY_TOKEN: '',
     API_AI_CLIENT_ACCESS_TOKEN: '',
     FB_APP_SECRET: 'Bradburn2017',
-    SERVER_URL: "https://polar-eyrie-86548.herokuapp.com/webhook",
+    SERVER_URL: "https://kenjibradburn.herokuapp.com/webhook",
+    
 };
